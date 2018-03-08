8598aeefd7d8f666c4ab6b5ea99e0bfe65a39f1a
Mark Griffith <mgriffit@ualberta.ca>
2018-03-08 10:21:10 -0700

    Add utils

---
+++ utils/factories/colors.lua
@@ -0,0 +1,12 @@
+
+local colors = {}
+
+function colors.fromHex(hex)
+  return { hex=hex, alpha=1 }
+end
+
+function colors.fromHexAlpha(hex, alpha)
+  return pl.tablex.update(colors.fromHex(hex), { alpha=alpha })
+end
+
+return colors
